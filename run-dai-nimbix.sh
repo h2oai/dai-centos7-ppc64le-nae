63b189d0367b28c7c5142cfcb5a08f4a19ebf040
Jeff Gambera <gambera@gmail.com>
2018-06-21 06:31:03 -0700

    simplify start

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -3,28 +3,7 @@
 set -e
 set -x
 
-export DRIVERLESS_AI_CONFIG_FILE_PATH="/opt/h2oai/dai"
-echo "$DRIVERLESS_AI_CONFIG_FILE_PATH"
+export DRIVERLESS_AI_AUTHENTICATION_METHOD="local"
+export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
 
-if [ -z "$5" ]
-then
-  echo "No Configuration File Provided"
-  export DRIVERLESS_AI_AUTHENTICATION_METHOD="local"
-  export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
-else
-  echo "Making Configuration File Available for DAI"
-  CONFIG_FILE="$5"
-  cp $CONFIG_FILE "$DRIVERLESS_AI_CONFIG_FILE_PATH/config.toml"
-fi
-
-echo "Starting Driverless AI"
-
-if [ $4 -gt 0 ]
-then
-  sudo nvidia-smi -pm 1
-else
-  echo "No GPUs Available, CPU Only"
-fi
-
-/sbin/init
 sudo tail -f /opt/h2oai/dai/log/dai.out
