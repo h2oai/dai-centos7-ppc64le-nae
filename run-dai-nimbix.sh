e92b54bcf2befa01ceff1c148e13a86637466681
Jeff Gambera <gambera@gmail.com>
2018-06-21 13:32:52 -0700

    workaround for dbus issues with nvidia ppc

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -5,5 +5,5 @@ export DRIVERLESS_AI_AUTHENTICATION_METHOD="local"
 export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
 
 sudo nvidia-smi -pm 1
-sudo systemctl restart dai
+sudo /opt/h2oai/dai/run-dai.sh
 sudo tail -f /opt/h2oai/dai/log/dai.out
