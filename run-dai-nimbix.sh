71aa9ed694520733681ee2f13d30b3446fcf4ff4
Jeff Gambera <gambera@gmail.com>
2018-06-21 08:47:38 -0700

    remove log from run script

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -8,4 +8,4 @@ export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
 
 sudo nvidia-smi -pm 1
 sudo systemctl restart dai
-sudo tail -f /opt/h2oai/dai/log/dai.out
+#sudo tail -f /opt/h2oai/dai/log/dai.out
